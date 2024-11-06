d17c7e00681c2ee7f8916460b3034011bb1f2374
RoMadrigal <rmadrigalmontes@gmail.com>
2024-11-06 01:14:42 -0600

    Agregando archivo de implementacion y corrigiendo error en verificarExistencia.hpp
    Signed-off-by: RoMadrigal <rmadrigalmontes@gmail.com>

--- include/verificarExistencia.hpp
+++ include/verificarExistencia.hpp
@@ -36,21 +36,21 @@ bool cuentaExiste(sqlite3* db, int idCuenta);
  * @brief Función para verificar si un cliente existe en la base de datos
  * 
  * @param db Puntero a la base de datos
- * @param idCuenta Id del préstamo que se quiere verificar
+ * @param idPrestamo Id del préstamo que se quiere verificar
  * @return true Si el registro del préstamo no existe en la base de datos
  * @return false 
  */
-bool prestamoExiste(sqlite3* db, int idCuenta);
+bool prestamoExiste(sqlite3* db, int idPrestamo);
 
 /**
  * @brief Función para verificar si un cliente existe en la base de datos
  * 
  * @param db Puntero a la base de datos
- * @param idCuenta Id del CDP que se quiere verificar
+ * @param idCDP Id del CDP que se quiere verificar
  * @return true Si el registro del CDP existe en la base de datos
  * @return false Si el registro del CDP no existe en la base de datos
  */
-bool cdpExiste(sqlite3* db, int idCuenta);
+bool cdpExiste(sqlite3* db, int idCDP);
 
 
 #endif // EXISTENCE_CHECKS_HPP
