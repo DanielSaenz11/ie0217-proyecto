1808cea40180a141c13817d94e41be36f09a5680
Daniel Sáenz <dsaenzobando@gmail.com>
2024-11-08 20:19:19 -0600

    Agregando documentacion en Doxygen para Database
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- include/Database.hpp
+++ include/Database.hpp
@@ -1,4 +1,15 @@
-
+/**
+ * @file Database.hpp
+ * @brief Declaración de la clase Database para gestionar la conexión a la base de datos.
+ * @details Este archivo contiene la declaración de la clase Database, que facilita la gestión
+ *          de la conexión a una base de datos SQLite, permitiendo abrir y cerrar la conexión
+ *          de forma segura.
+ * 
+ * @author Daniel Alberto Sáenz Obando
+ * @author Rodrigo Madrigal Montes
+ * @date 08/11/2024
+ * @copyright MIT License
+ */
 
 #ifndef DATABASE_HPP
 #define DATABASE_HPP
@@ -9,17 +20,21 @@
 /**
  * @brief Clase que gestiona la conexión a la base de datos.
  * 
- * Esta clase permite abrir y cerrar la conexión a la base de datos SQLite.
+ * Esta clase permite abrir y cerrar la conexión a la base de datos SQLite. 
+ * Al crear un objeto de esta clase, se abre la conexión, y al destruir el objeto, 
+ * la conexión se cierra automáticamente.
  */
 class Database {
     private:
-        sqlite3* db; /**< Puntero a la conexión de base de datos SQLite */
+        /// @brief Puntero a la conexión de base de datos SQLite.
+        sqlite3* db;
 
     public:
         /**
          * @brief Constructor de la clase Database.
          * 
-         * Inicializa y abre la conexión a la base de datos especificada.
+         * Inicializa y abre la conexión a la base de datos especificada. Si no se puede
+         * abrir la base de datos, el constructor maneja el error apropiadamente.
          * 
          * @param dbName Nombre de la base de datos a abrir.
          */
@@ -28,9 +43,17 @@ class Database {
         /**
          * @brief Destructor de la clase Database.
          * 
-         * Cierra la conexión a la base de datos al destruir el objeto.
+         * Cierra la conexión a la base de datos al destruir el objeto, liberando los recursos
+         * utilizados.
          */
         ~Database();
+
+        /**
+         * @brief Retorna un puntero a la conexión de la base de datos en SQLite
+         * 
+         * @return `sqlite3*` Puntero a la conexión
+         */
+        sqlite3* get() const;
 };
 
 #endif // DATABASE_HPP
