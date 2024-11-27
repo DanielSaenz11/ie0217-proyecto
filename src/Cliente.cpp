57ace0f92e7d451933444e7f1c80796bca6a170a
Daniel Sáenz <dsaenzobando@gmail.com>
2024-11-26 22:05:58 -0600

    Agregando manejo de errores en Cliente
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -11,6 +11,7 @@
  */
 
 #include "Cliente.hpp"
+#include "SQLiteStatement.hpp"
 #include <iostream>
 
 // Constructor para inicializar un cliente con los datos proporcionados.
@@ -21,110 +22,100 @@ Cliente::Cliente(int cedula, const std::string &nombre, const std::string &prime
 
 // Función para crear un nuevo registro de cliente en la base de datos.
 bool Cliente::crear(sqlite3* db) {
-
-    // Verificar que no exista un cliente con el mismo número de cédula.
+    // Verificar que no exista un cliente con el mismo número de cédula
     if (Cliente::existe(db, this->cedula)) {
-        std::cout << "Error: Ya existe un cliente con el número de cédula" << std::endl;
-        return false; // Salir de la función si el cliente ya existe
-    }
-
-    // Consulta SQL para insertar un nuevo cliente en la base de datos
-    std::string sql = "INSERT INTO Clientes (cedula, nombre, primerApellido, segundoApellido, telefono) VALUES (?, ?, ?, ?, ?);";
-    sqlite3_stmt* stmt; // Declaración del statement
-
-    // Preparar la consulta SQL
-    if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
-        std::cerr << "Error al preparar la consulta: " << sqlite3_errmsg(db) << std::endl;
-        sqlite3_finalize(stmt); // Liberar memoria del statement en caso de error
+        std::cerr << "Error: Ya existe un cliente con el número de cédula" << std::endl;
         return false;
     }
 
-    // Asignar valores a los parámetros de la consulta
-    sqlite3_bind_int(stmt, 1, cedula);
-    sqlite3_bind_text(stmt, 2, nombre.c_str(), -1, SQLITE_STATIC);
-    sqlite3_bind_text(stmt, 3, primerApellido.c_str(), -1, SQLITE_STATIC);
-    sqlite3_bind_text(stmt, 4, segundoApellido.c_str(), -1, SQLITE_STATIC);
-    sqlite3_bind_text(stmt, 5, telefono.c_str(), -1, SQLITE_STATIC);
+    // Consulta SQL para insertar un nuevo cliente en la base de datos
+    const std::string sql = "INSERT INTO Clientes (cedula, nombre, primerApellido, segundoApellido, telefono) VALUES (?, ?, ?, ?, ?);";
 
-    // Ejecutar la inserción en la base de datos
-    bool exito = (sqlite3_step(stmt) == SQLITE_DONE);
+    try {
+        SQLiteStatement statement(db, sql);
 
-    if (exito) {
+        // Asignar valores a los parámetros de la consulta
+        sqlite3_bind_int(statement.get(), 1, cedula);
+        sqlite3_bind_text(statement.get(), 2, nombre.c_str(), -1, SQLITE_STATIC);
+        sqlite3_bind_text(statement.get(), 3, primerApellido.c_str(), -1, SQLITE_STATIC);
+        sqlite3_bind_text(statement.get(), 4, segundoApellido.c_str(), -1, SQLITE_STATIC);
+        sqlite3_bind_text(statement.get(), 5, telefono.c_str(), -1, SQLITE_STATIC);
+
+        // Ejecutar la inserción en la base de datos
+        if (sqlite3_step(statement.get()) != SQLITE_DONE) {
+            throw std::runtime_error("Error al insertar en la base de datos: " + std::string(sqlite3_errmsg(db)));
+        }
+
         // Obtener el ID del cliente recién insertado y asignarlo al atributo idCliente
         idCliente = sqlite3_last_insert_rowid(db);
         std::cout << "Cliente creado con ID: " << idCliente << std::endl;
-    } else {
-        std::cerr << "Error al insertar en la base de datos: " << sqlite3_errmsg(db) << std::endl;
-    }
-    
-    sqlite3_finalize(stmt); // Liberar memoria del statement
+        return true;
 
-    return exito; // Retornar estado de inserción
+    } catch (const std::exception& e) {
+        // Manejar errores y reportar mensajes en consola
+        std::cerr << e.what() << std::endl;
+        return false;
+    }
 }
 
 // Función para obtener un cliente desde la base de datos por medio de su cédula
 Cliente Cliente::obtener(sqlite3* db, int cedula) {
     // Consulta SQL para seleccionar datos del cliente a partir de su cédula
     std::string sql = "SELECT idCliente, nombre, primerApellido, segundoApellido, telefono FROM Clientes WHERE cedula = ?;";
-    sqlite3_stmt* stmt;
 
     // Crear un cliente vacío que se retornará si no se encuentra el cliente en la base de datos
     Cliente cliente(0, "", "", "", "");
 
-    // Preparar la consulta SQL
-    if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
-        std::cerr << "Error al preparar la consulta: " << sqlite3_errmsg(db) << std::endl;
-        sqlite3_finalize(stmt); // Liberar memoria del statement en caso de error
-        return cliente; // Salir y retornar cliente vacío en caso de error
+    try {
+        SQLiteStatement statement(db, sql);
+
+        // Asignar el valor de la cédula al parámetro de la consulta
+        sqlite3_bind_int(statement.get(), 1, cedula);
+
+        // Ejecutar la consulta y verificar si se encontró un registro
+        if (sqlite3_step(statement.get()) == SQLITE_ROW) {
+            // Asignar los valores obtenidos de la base de datos al objeto cliente
+            cliente.idCliente = sqlite3_column_int(statement.get(), 0);
+            cliente.cedula = cedula;
+            cliente.nombre = reinterpret_cast<const char*>(sqlite3_column_text(statement.get(), 1));
+            cliente.primerApellido = reinterpret_cast<const char*>(sqlite3_column_text(statement.get(), 2));
+            cliente.segundoApellido = reinterpret_cast<const char*>(sqlite3_column_text(statement.get(), 3));
+            cliente.telefono = reinterpret_cast<const char*>(sqlite3_column_text(statement.get(), 4));
+        } else {
+            throw std::runtime_error("Error: Cliente no encontrado con la cédula ingresada.");
+        }
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << std::endl;
     }
 
-    // Asignar el valor de la cédula al parámetro de la consulta
-    sqlite3_bind_int(stmt, 1, cedula);
-
-    // Ejecutar la consulta y verificar si se encontró un registro
-    if (sqlite3_step(stmt) == SQLITE_ROW) {
-        // Asignar los valores obtenidos de la base de datos al objeto cliente
-        cliente.idCliente = sqlite3_column_int(stmt, 0);
-        cliente.cedula = cedula;
-        cliente.nombre = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
-        cliente.primerApellido = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2));
-        cliente.segundoApellido = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 3));
-        cliente.telefono = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4));
-    } else {
-        std::cerr << "Error: Cliente no encontrado con cédula ingresada." << std::endl;
-    }
-
-    sqlite3_finalize(stmt); // Liberar memoria del statement
-    return cliente; // Retornar el objeto cliente (vacío si no se encontró)
+    return cliente;
 }
 
 // Función para verificar si existe un cliente en la base de datos con la cédula
 bool Cliente::existe(sqlite3* db, int cedula) {
     // Consulta SQL para verificar la existencia de un cliente mediante su cédula
     const std::string sql = "SELECT COUNT(1) FROM Clientes WHERE cedula = ?;";
-    sqlite3_stmt* stmt;
 
-    // Condición de existencia
-    bool existe = false;
+    try {
+        // Utilizar SQLiteStatement para manejar el ciclo de vida del statement
+        SQLiteStatement statement(db, sql);
 
-    // Preparar la consulta SQL
-    if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
-        std::cerr << "Ocurrió un error al preparar la consulta para Cliente: " << sqlite3_errmsg(db) << std::endl;
-        sqlite3_finalize(stmt); // Liberar memoria del statement en caso de error
-        return false;
+        // Asignar el valor de la cédula al parámetro de la consulta
+        sqlite3_bind_int(statement.get(), 1, cedula);
+
+        // Ejecutar la consulta y obtener el resultado
+        if (sqlite3_step(statement.get()) == SQLITE_ROW) {
+            // Retornar true si el resultado es mayor a 0
+            return sqlite3_column_int(statement.get(), 0) > 0;
+        } else {
+            throw std::runtime_error("Error al ejecutar la consulta para verificar la existencia del cliente.");
+        }
+
+    } catch (const std::exception& e) {
+        // Manejar errores y reportar mensajes en consola
+        std::cerr << e.what() << std::endl;
+        return false; // Retornar false en caso de error
     }
-
-    // Asignar el valor de la cédula al parámetro de la consulta
-    sqlite3_bind_int(stmt, 1, cedula);
-
-    // Ejecutar la consulta y obtener el resultado de existencia
-    if (sqlite3_step(stmt) == SQLITE_ROW) {
-        existe = sqlite3_column_int(stmt, 0); // Almacenar el resultado en la variable existe
-    }
-
-    sqlite3_finalize(stmt); // Liberar memoria del statement
-    
-    return existe; // Retornar el resultado de existencia
 }
 
 // Función que retorna la cédula del cliente
